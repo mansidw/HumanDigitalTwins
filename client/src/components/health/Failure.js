929ef1628e98f9c7454064648dcca8e1e2318599
mansidw <mansi.dwivedi@spit.ac.in>
2021-12-02 11:07:06 +0530

    final project done

--- client/src/components/health/Failure.js
+++ client/src/components/health/Failure.js
@@ -31,13 +31,30 @@ useEffect(async ()=>{
     
 },[])
     
-function handleSubmit(cal){
-    let met = cal/(timeRef.current.value*weight)
+function handleSubmit(e,cal){
+    e.preventDefault();
     alert(weight)
-    alert(met)
     
+    if((weight<=60 || weight<=100) && cal<=500 ){
+        setArr((old)=>[...old,{'name':'Aerobics, general'},{'name':'Aerobics, high impact'},{'name':'Archery'},
+        {'name':'Badminton'},{'name':'Ballroom dancing, fast'},{'name':'Bathing dog'},{'name':'BadBoxing, punching bagminton'},{'name':'Canoeing, rowing, light'},
+        {'name':'Carrying moderate loads upstairs'},{'name':'Cross country skiing, moderate'},{'name':'BadmintCycling, 10-11.9mph, lighton'}])
+    }
+    else if((weight<=60 || weight<=100) && cal>=500){
+        alert("yea")
+        setArr((old)=>[...old,{'name':'Fire fighter, climbing ladder, full gear'},{'name':'Football, competitive'},{'name':'Forestry, ax chopping, fast'},
+        {'name':'Handball'},{'name':'Judo, karate, jujitsu, martial arts'},{'name':'Jumping rope, moderate'},{'name':'Kick boxing'},{'name':'Orienteering'},
+        {'name':'Paddleball, competitive'},{'name':'Running, 7.5mph (8 min mile)'},{'name':'Shoveling, digging ditches'}])
+    }
+    else {
+
+        setArr((old)=>[...old,{'name':'Aerobics, step aerobics'},{'name':'Ballet, twist, jazz, tap'},{'name':'Basketball, shooting baskets'},
+        {'name':'Carrying 25 to 49 lbs, upstairs'},{'name':'Cleaning gutters'},{'name':'Crew, sculling, rowing, competition'},{'name':'Cycling, 16-19mph, very fast, racing'},{'name':'Forestry, trimming trees'},
+        {'name':'Gymnastics'},{'name':'Hunting, small game'},{'name':'Kayaking'}])
+    }
+    // return <FailureCards arr={arr}/>
     // const metsRef = firestore.collection('mets');
-    // const snapshot = await metsRef.where('Value', '<=','3').orderBy('Value').limit(3).get();
+    // const snapshot = await metsRef.where('Value', '<=','3.0').orderBy('Value').limit(3).get();
     // if (snapshot.empty) {
     //     console.log('No matching documents.');
     // }  
@@ -72,7 +89,7 @@ const fillcaldb = async (id)=>{
                             
                         </div>
                         <div className="div5">
-                            <form onSubmit={()=>handleSubmit(props.cal)}>
+                            <form onSubmit={(e)=>handleSubmit(e,props.cal)}>
                                 <Stack direction="row" spacing={2}>
                             
                                     <div><TextField style={{marginTop:'20px',width:'200px'}} type="number" id="outlined-basic" label="How much?" variant="outlined" inputRef={timeRef} /></div>
@@ -86,7 +103,7 @@ const fillcaldb = async (id)=>{
                     </div>
                     <h1 style={{fontSize:'30px'}}>The calories you still need to burn is <span style={{lineHeight:'50px',fontWeight:'bold'}}>{props.cal}</span> calories!</h1>
                     <Box sx={{ width:'50%',marginLeft:'350px', marginTop:'70px' }}>
-                        {arr?<FailureCards arr={arr}/>:<></>}
+                        <FailureCards arr={arr}/>
                     </Box>
                     <Button variant="contained" type="button" onClick={()=>fillcaldb(props.id)} style={{width:'150px',height:'60px',fontFamily:'inherit',fontWeight:'bold',fontSize:'20px',backgroundColor:'darkblue',color:'white',marginTop:'15px'}}>Back to Home</Button>  
                         
